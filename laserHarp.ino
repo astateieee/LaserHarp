748b1f090c2b7a923a2498de4292713584ba5378
Darrell Henderson <darrell@thingable.com>
2016-01-31 11:38:21 -0600

    Laser is lasing

--- laserHarp.ino
+++ laserHarp.ino
@@ -2,11 +2,11 @@
 const int laser = 6;
 
 // initialize the stepper library on pins 8 through 11:
-Stepper myStepper(324, 12, 11, 10, 9);
+Stepper myStepper(324, 8, 9, 10, 11);
 
 void setup() {
   myStepper.setSpeed(80);
-  Serial.begin(9600);
+  //Serial.begin(9600);
   pinMode(laser, OUTPUT);
 }
 
@@ -15,10 +15,11 @@ void loop() {
   //Serial.println(analogRead(A1));
   
   for(int i = 0; i < 12; i++){
-    myStepper.step(2, laser, 100);
+    myStepper.step(1, laser);
+    
   }
-
+  
   for(int i = 0; i < 12; i++){
-    myStepper.step(-2, laser, 100);
+    myStepper.step(-1, laser);
   }
 }
